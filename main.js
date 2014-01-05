58a21fc128812e611e16eca74e2f18a24985b15f
nate dawg <nglass1988@gmail.com>
2014-01-04 21:56:18 -0800

    cleaned up some crap

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ var photoMap = {
 
 
 "initialize":function() {
-console.log("adnfkjsndfksd");
        var mapOptions = {
            center: new google.maps.LatLng(37.4419, -102.1419),
            zoom: 5
@@ -19,9 +18,6 @@ console.log("adnfkjsndfksd");
 
        if (res.authResponse) {
            photoMap.token = res.authResponse.accessToken;
-           // updateUserInfo(res);
-
-           photoMap.drawMap();
            photoMap.getUserFriends();
        }
    },
@@ -31,7 +27,6 @@ console.log("adnfkjsndfksd");
       var url = '/me/friends?fields=name,picture&access_token=' + photoMap.token
 
       FB.api(url, function(response) {
-// console.log(response);
           if (!response.error) {
               var markup = '';
               var friends = response.data;
@@ -46,40 +41,9 @@ console.log("adnfkjsndfksd");
                   $(this).toggleClass('selected');
                   photoMap.getUserPhotos($(this).attr('id'));
               });
-//console.log($imgs)
-
-/*
-              $('button.photos').click(function(e){
-                  photoMap.getUserPhotos($(this).attr('title'));
-              });
-*/
           }
       });
   },
-
-
-
-  "markerLayerHandler":function(e) {
-// console.log(e);
-// =*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*
-//        var marker = e.layer;
-//        var feature = marker.feature;
-//        var image = feature.properties.images
-//        // var img = images
-                                
-// // console.log(image);
-//        // Create custom popup content
-//        var popupContent = '<img src="' + image + '" />'
-//        marker.bindPopup(popupContent,{
-//            closeButton: false,
-
-//            offset:[180, 20]
-//        });
-// =*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*
-
-  },
-
-
 
    "getUserPhotos":function(id) {
        var url = '/'+id+'/albums?fields=photos&access_token=' + photoMap.token;
@@ -135,19 +99,7 @@ console.log("adnfkjsndfksd");
 
 
    "placeMarker":function() {
-
-
-
-
        function createMarker(photoset) {
-
-
-           // if(photoMap.markers.length > 0) {
-           //     for (var i = 0; i < photoMap.markers.length; i++ ) {
-           //         photoMap.markers[i].setMap(null);
-           //     }
-           // }
-
 
            var markers = [];
            $.each(photoset, function(i) {
@@ -160,9 +112,6 @@ console.log("adnfkjsndfksd");
                    title: 'Hello World!'
                });
 
-               // markers.push(marker);
-
-console.log(this);
                var contentString = this.name +'<br /><img src="'+this.src+'" />';
 
                var infowindow = new google.maps.InfoWindow({
@@ -179,112 +128,22 @@ console.log(this);
                    infowindow.open(photoMap.map, marker);
                });
            });
-
-           // photoMap.markers = markers;
-
-
-
-
        }
-
-
-
-
-
-
        $('article img.selected').each(function(){
-
-
            var photos = $.data(this).photos;
-
-
            if(photos){
                createMarker(photos);                
            }
-
-
-// console.log(this);
-// console.log();
-// console.log($.data(this).photos);
-//console.log(photos$.data(this));
-
-
-           
-
-
-          // photoset = photoset.concat($.data(this).photos)
        });
-
-
-
-
-
-/*
-
-
-      if (photos.length > 0) {
-      for (var i = 0; i < photos.length; i++) {
-   var place = photos[i];
-
-   var myLatLng = new google.maps.LatLng(place[2], place[1]);
-   var marker = new google.maps.Marker({
-       position: myLatLng,
-       map: photoMap.map
-       // icon: image,
-       // shape: shape,
-       // title: beach[0],
-       // zIndex: beach[3]
-   });
- 	 }
-      }
-*/       
-// photoset = [{ 
-//   "type": "FeatureCollection",
-//   "features": photoset
-// }];
-
-      // photoMap.map.markerLayer.on('layeradd', photoMap.markerLayerHandler);
-
-      // set photo markers
-      // photoMap.map.markerLayer.setGeoJSON(photoset);
-
-      // photoMap.map.markerLayer.on('click', function(e) {
-      //     e.layer.openPopup(); 
-      //     var lat = e.layer.getLatLng().lat;
-      //     var lng = e.layer.getLatLng().lng;
-      //     var pixels = photoMap.map.latLngToContainerPoint([lat, lng]);
-
-      //     photoMap.map.panTo(photoMap.map.containerPointToLatLng([pixels.x + 280, pixels.y - 230]), 2);
-      // });
-      // $('article').animate({left:"-260px"});
   },
 
   "loginUser":function() {
       FB.login(function(response) { }, {"scope":"read_friendlists,user_photos,friends_photos"});
-      // photoMap.drawMap();
-  },
-
-  "drawMap":function() {
-      // photoMap.map = L.mapbox.map('map', 'natecraft1.5l95dn29').setView([0.00, 0.00], 3);
-// console.log('test')
-      
   },
 
 
   "_init":function() {
-//console.log( $('body').hasClass('connected') );
-//console.log(this);
-// console.log($('body').hasClass('connected'));
-
-       if($('body').hasClass('connected')) {
-           photoMap.drawMap();
-
-       }
-
-       else {
-           $('#login').find('button').click(photoMap.loginUser);
-       }
-              
+      $('#login').find('button').click(photoMap.loginUser);     
   }
 }
 
